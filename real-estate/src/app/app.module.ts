8beeecb9b14c322bffb1f0b3aa3ed9f31411a582
Anton Golenok <anton_post@mail.ru>
2022-08-17 00:44:56 +0300

    Added layout and gallery components

--- real-estate/src/app/app.module.ts
+++ real-estate/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InvestComponent } from './components/invest/invest.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InvestComponent
+    InvestComponent,
+    LayoutComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule
